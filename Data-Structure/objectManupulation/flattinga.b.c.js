fd7a5db068ebb6ed2ade8de1eb3c4255e4415cae
rishsri <57784484+rishsri@users.noreply.github.com>
2024-10-08 09:59:23 +0530

    Update flattinga.b.c.js

--- Data-Structure/objectManupulation/flattinga.b.c.js
+++ Data-Structure/objectManupulation/flattinga.b.c.js
@@ -2,35 +2,24 @@ const nested = {
   a: "12",
   b: 23,
   c: {
-      p: 23,
-      z: {
-          l: [24, 34]
-      }
-  }
-};
+    p: 23,
+    z: {
+      l: [24, 34],
+    },
+  },
+}
 
-const modifyObj = (obj, prefix) => {
-  let newObj = {};
+const objectFlat = (obj, prefix = "", result = {}) => {
+
   for (let i in obj) {
-      let newPrefix = prefix ? prefix + "." + i : i;
-
-      if (typeof obj[i] === "object") {
-          if (Array.isArray(obj[i])) {
-              // Iterate over array elements and add them to newObj
-              for (let j = 0; j < obj[i].length; j++) {
-                  newObj[newPrefix + "." + j] = obj[i][j];
-              }
-          } else {
-              // Recursively process nested objects
-              const newMethod = modifyObj(obj[i], newPrefix);
-              newObj = { ...newObj, ...newMethod };
-          }
-      } else {
-          // Add primitive values to newObj
-          newObj[newPrefix] = obj[i];
+    let fullfilled = prefix ? prefix + "." + i : i
+    	if(typeof obj[i] === "object" && obj[i] !== null) {
+      	objectFlat(obj[i], fullfilled, result)
+      }else {
+      	result[fullfilled] = obj[i]
       }
-  }
-  return newObj;
-};
+   }
+  return result
+}
 
-console.log(modifyObj(nested));
+console.log(objectFlat(nested))
