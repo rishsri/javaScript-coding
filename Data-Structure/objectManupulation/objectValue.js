6d159bf0db745eaabc68bf3e2ee159b421d38270
rishsri <57784484+rishsri@users.noreply.github.com>
2024-10-08 10:10:17 +0530

    Update objectValue.js

--- Data-Structure/objectManupulation/objectValue.js
+++ Data-Structure/objectManupulation/objectValue.js
@@ -39,3 +39,24 @@ const bookArray = [{
 
 const booksByAuthor = groupBooksByAuthor(bookArray);
 console.log(booksByAuthor);
+
+// ----------------------------------------------------------
+
+const swapKeysAndValues = (value) => {
+  const obj = {};
+
+  for (let key in value) {
+    if (value.hasOwnProperty(key)) {
+      if (obj[value[key]]) {
+        obj[value[key]].push(key);
+      } else {
+        obj[value[key]] = [key];
+      }
+    }
+  }
+  return obj;
+};
+
+const exampleObject = { a: 'apple', b: 'banana', c: 'apple', d: 'date' };
+const swappedObject = swapKeysAndValues(exampleObject);
+console.log(swappedObject);
